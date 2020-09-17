e339b59fa4f82f388efb47e7e2d56db109a36a3d
Dan Finlay <dan@danfinlay.com>
2020-09-16 17:06:36 -0700

    Fix nested dist folders

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     extensions: [ '.tsx', '.ts', '.js' ],
   },
   output: {
-    filename: 'dist/index.cjs.js',
+    filename: 'index.cjs.js',
     path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
