59cff2b4c34508fa024e752257bfb21796d4ffcf
Erik Marks <rekmarks@protonmail.com>
2021-04-28 14:58:50 -0700

    Add comments

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,12 @@ module.exports = {
   },
   moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node'],
   preset: 'ts-jest',
+  // "resetMocks" resets all mocks, including mocked modules, to jest.fn(),
+  // between each test case.
   resetMocks: true,
+  // "restoreMocks" restores all mocks created using jest.spyOn to their
+  // original implementations, between each test. It does not affect mocked
+  // modules.
   restoreMocks: true,
   testEnvironment: 'node',
   testRegex: ['\\.test\\.(ts|js)$'],
